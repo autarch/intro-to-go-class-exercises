8b8a8f44f1862843d5db072cab1dade0d158cfc7
Dave Rolsky <autarch@urth.org>
2015-06-01 20:34:51 -0500

    Fix typo in interface name

--- 9-more-interfaces/http/http.go
+++ 9-more-interfaces/http/http.go
@@ -3,7 +3,7 @@
 The goal for this exercise is to create an interface that represents HTTP
 errors. This interface will also implement the built-in Error interface.
 
-Call this interface HTTPError. It should require the Error interface (Error()
+Call this interface httpError. It should require the Error interface (Error()
 string) as well as a method called HTTPStatus() which returns a uint value (an
 HTTP status code).
 
@@ -20,13 +20,13 @@ Create constructors named NewHTTPBadRequestError() and NewHTTPNotFoundError()
 for those two types. The message for each error can be anything you want (but
 don't leave it empty).
 
-Each of those types should implement the HTTPError interface specified above.
+Each of those types should implement the httpError interface specified above.
 
 Finally, create a function named ErrorResponse() that accepts *any* error type
 and returns a uint and a string. The uint is an HTTP status and the string the
-error message. If the error is *not* an HTTPError, return 500 for the status
+error message. If the error is *not* an httpError, return 500 for the status
 and the error's Error() as the message. Otherwise return the status and
-message from the HTTPError.
+message from the httpError.
 
 You should use either type assertions or the type switch construct to
 implement the ErrorResponse() function.
